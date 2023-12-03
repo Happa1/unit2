776d9b1e7ad1eae0e294d6a7beabf3eeba04d73a
Happa1 <2025.manaha.ueda@uwcisak.jp>
2023-12-03 16:43:10 +0900

    push onegaii

---
+++ Project/graph1.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+from graph_lib import draw_graph, smoothing,basic_info
+
+with open('testcase.csv', mode='r') as f:
+    data = f.readlines()
+    temp1 = []
+    humid1 = []
+    temp2 = []
+    humid2 = []
+    temp3 = []
+    humid3 = []
+    t=0
+    time=[]
+    for line in data:
+        t1 = line.split(',')[1]
+        t2 = line.split(',')[2]
+        t3 = line.split(',')[3]
+        h1 = line.split(',')[4]
+        h2 = line.split(',')[5]
+        h3 = line.split(',')[6].strip()
+
+        temp1.append(float(t1))
+        temp2.append(float(t2))
+        temp3.append(float(t3))
+        humid1.append(float(h1))
+        humid2.append(float(h2))
+        humid3.append(float(h3))
+        time.append(t)
+        t+=1
+
+    print(data)
+
+
+print(time)
+print(temp1)
+# graph of temp1
+# temp1_graph=draw_graph(t=time, v=temp1, color='black', title="temp1")
+# plt.show()
+
+#smoothing graph of temp1
+# temp1_smoothing = draw_graph(t=time, v=smoothing(x=temp1, size_window=5))
+# plt.show()
+#
+# print(f"temp1:{temp1}")
+# print(f"temp2:{temp2}")
+# print(f"temp3:{temp3}")
+
+
+# avg graph of temp1
+temp1_avg=basic_info(x=temp1, y=temp2, z=temp3)
+print(f"mean:{temp1_avg[0]}")
+print(f"std: {temp1_avg[1]}")
+print(f"min: {temp1_avg[2]}")
+print(f"{temp1_avg[3]}")
+# print(temp1_avg[4])
+
+
+temp_avg = draw_graph(t=time, v=temp1_avg[4],color='black', title="temp1")
+plt.show()
---
+++ Project/graph_lib1.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import numpy as np
+
+plt.style.use('ggplot')
+
+# draw graph
+def draw_graph (t:list[int], v:list[int], color:str, title:str):
+    plt.plot(t, v, color=f"{color}")
+    plt.title(title)
+
+# smoothing
+def smoothing(x:list[int], size_window:int=5):
+    smooth_x = []
+    for i in range(0, len(x), size_window):
+        points=sum(x[i : i + size_window]) / size_window
+        smooth_x.append(points)
+    return smooth_x
+
+# average
+def basic_info (x:list, y:list, z:list):
+    total=[]
+    mean = []
+    std = []
+    min_val = []
+    max_val = []
+    avg = []
+
+    for i in range(len(x)):
+        total.append([x[i],y[i],z[i]])
+        # print(total)
+        # print(total[i])
+
+    for item in total:
+        mean.append(np.mean(item)) #0
+        std.append(np.std(item)) #1
+        min_val.append(np.min(item)) #2
+        max_val.append(np.max(item)) #3
+        avg.append(np.sum(x)/3) #4
+
+    return mean, std, min_val, max_val
